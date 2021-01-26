ad39ca49056620b14fc0a2101d3e7126697f93dc
paulTAI <paul.taitinger@open-groupe.com>
2021-01-26 18:20:07 +0100

    localcheck

--- src/utils/provider.js
+++ src/utils/provider.js
@@ -1,6 +1,6 @@
 const provider = {
-    url : {
-        API_PLATEFORM: "https://api-platform-esgi.herokuapp.com",
+    url: {
+        API_PLATEFORM: "https://localhost:8443/api/v1",
         GRAPHQL: "https://graphql-projet.herokuapp.com/",
         WEBSOCKET_GRAPHQL: "wss://graphql-projet.herokuapp.com/"
     }

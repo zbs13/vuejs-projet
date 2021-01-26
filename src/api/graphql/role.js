1c3f96888f5cf8e1e63adce2ea068586fba6304d
paulTAI <paul.taitinger@open-groupe.com>
2021-01-26 19:53:12 +0100

    add request creatRoles

--- src/api/graphql/role.js
+++ src/api/graphql/role.js
@@ -1,12 +1,12 @@
-import {reqGraphQL} from '../request';
+import { reqGraphQL } from '../request';
 import gql from 'graphql-tag';
 import store from "../../store/app";
 
 export const role = {
-    getUserRolesGroup: function(groupId) {
-        return reqGraphQL(
-            'query',
-            gql`query($id:ID!,$groupId: ID!){
+  getUserRolesGroup: function (groupId) {
+    return reqGraphQL(
+      'query',
+      gql`query($id:ID!,$groupId: ID!){
                 roles(
                   where:{
                     AND:{
@@ -28,18 +28,83 @@ export const role = {
                     }
                     
                 }
-              }`, 
-              {
-                id: window.localStorage.getItem("user_id"),
-                groupId: groupId
-              },
-            'Récupération des roles en cours...',
-            true,
-            function(res){
-                return res.roles;
+              }`,
+      {
+        id: window.localStorage.getItem("user_id"),
+        groupId: groupId
+      },
+      'Récupération des roles en cours...',
+      true,
+      function (res) {
+        return res.roles;
+      }
+    )
+  },
+  createRole: function ({ name, groupId }) {
+    reqGraphQL(
+      'mutation',
+      gql`mutation($name: String!, $userId: ID!, $groupId: ID!){
+            createRole(
+              data: {
+                name: $name,
+                toUser:{
+                  connect:{
+                      id: $userId
+                  },
+                  toGroup:{
+                    connect:{
+                        id: $groupId
+                    }
+                }
+              }
+            ),
+            {
+              id
             }
-        )
-    }
-   
-    
+          }`,
+      {
+
+        name: name,
+        userId: window.localStorage.getItem("user_id"),
+        groupId: groupId
+      },
+      'Création du role cours...',
+      true,
+      function () {
+        store.dispatch("addPopup", {
+          type: "success",
+          message: "Votre role a été crée"
+        });
+        window.location = "/groups";
+      }
+    )
+  },
+  deleteRole: function (roleId) {
+    reqGraphQL(
+      'mutation',
+      gql`mutation($roleId: ID!){
+            deleteRole(
+                where:{
+                    id: $roleId
+                }
+            ),
+            {
+                id
+            }
+          }`,
+      {
+        roleId: roleId
+      },
+      'Suppression du role en cours...',
+      true,
+      function () {
+        store.dispatch("addPopup", {
+          type: "success",
+          message: "Votre role a été supprimé"
+        });
+      }
+    )
+  },
+
+
 }

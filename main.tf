fa25b26625bc18d42be20be6307eaf7163653241
Lucas Estrade <lucas.estrade5@hotmail.fr>
2021-05-05 17:49:29 +0200

    terra

--- main.tf
+++ main.tf
@@ -20,11 +20,27 @@ terraform {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
 
 provider "aws" {
   region = "eu-west-1"
 }
 
 resource "aws_instance" "web" {
+  ami           = data.aws_ami.id
   instance_type = "t2.micro"
 }
